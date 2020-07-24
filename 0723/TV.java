e4f7355de7ed682d2eb244b865a182f5d601202c
CHOIDAAE <CHOIDAAE@M800>
2020-07-24 10:06:08 +0900

    07/23 과제입니다.

--- 0723/TV.java
+++ 0723/TV.java
@@ -1,6 +1,6 @@
 package com.ssafy.java.day4;
 
-public class TV {
+public class TV {// Java Beans: 멤버 변수는 private, 거기에 접근할 수 있는  getter/setter, default 생성자
 	private int num;
 	private String name;
 	private int price;
@@ -8,17 +8,21 @@ public class TV {
 	private int inch;
 	private String dType;
 	
-	public TV() {
-		super();
-	}
+	public TV() {}
 
-	public TV(int num, String name, int price, int inch, String dType) {
-		super();
+	public TV(int num, String name, int price, int qty, int inch, String dType) {
+		//생성자에서 명시적으로 this() 또는  super()를 호출하지 않으면 자동으로  super()삽입
+//		super(); 생성자의 첫번째 줄에서만 사용 가능
 		this.num = num;
 		this.name = name;
 		this.price = price;
+		this.qty = qty;
 		this.inch = inch;
 		this.dType = dType;
+	}
+	
+	public TV(int num, String name, int inch, String dType) {//this(): 자신의 다른 생성자 호출, 생성자의 맨 첫 줄에서만 사용 가능
+		this(num, name, 0, 0, inch, dType);
 	}
 
 	public int getNum() {
@@ -34,7 +38,10 @@ public class TV {
 	}
 
 	public void setName(String name) {
-		this.name = name;
+		if(name != null)
+			this.name = name;
+		else
+			System.out.println("이름을 입력해주세요.");
 	}
 
 	public int getPrice() {
@@ -42,7 +49,8 @@ public class TV {
 	}
 
 	public void setPrice(int price) {
-		this.price = price;
+		if(price > 0)
+			this.price = price;
 	}
 
 	public int getQty() {
@@ -73,6 +81,5 @@ public class TV {
 	public String toString() {
 		return "TV [제품 번호=" + num + ", 제품 이름=" + name + ", 가격=" + price + ", 수량=" + qty + ", 인치=" + inch + ", 디스플레이 타입=" + dType + "]";
 	}
-	
 	
 }
