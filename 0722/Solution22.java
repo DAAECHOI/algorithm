669b85982fb93ee53bdca412aad8e3814a280aa5
DAAECHOI <DAAECHOI@DESKTOP-NLHTQLF>
2020-07-26 22:05:33 +0900

    07/22 과제입니다.

--- 0722/Solution22.java
+++ 0722/Solution22.java
@@ -16,105 +16,76 @@ public class Solution22 {
 
 		System.setIn(new FileInputStream("Waterstrider2.txt"));
 		Scanner sc = new Scanner(System.in);
-		
-		T = sc.nextInt();
-		for(int t=1; t<=T; t++) {
-			N = sc.nextInt();
-			lake = new int[N][N];
-			S = sc.nextInt() + 1;
-			strider = new int[S][3];
-			
-			for(int i = 1; i < S; i++) {
-				strider[i][0] = sc.nextInt();
-				strider[i][1] = sc.nextInt();
-				strider[i][2] = sc.nextInt();
+
+		int T = sc.nextInt();
+		for(int t = 1; t <= T; t++) {
+			int N = sc.nextInt();
+			int[][] grid = new int[N][N];
+			int s = sc.nextInt();
+			int[][] ws = new int[s][3];
+			for(int i = 0; i < ws.length; i++) {
+				//ws.length==s ws[i].length == 0,1,2
+				ws[i][0] = sc.nextInt();//행
+				ws[i][1] = sc.nextInt();//열
+				ws[i][2] = sc.nextInt();//방향
 			}
-			
-			answer = S;
-			for(int i=1; i<S; i++) {
-				int row = strider[i][0];
-				int col = strider[i][1];
-				int dir = strider[i][2];
-				
-				if(lake[row][col]==1) {
-					answer--;
+			int cnt = s;
+			for(int i = 0; i < ws.length; i++) {
+				int x = ws[i][0];
+				int y = ws[i][1];
+				int dir = ws[i][2];
+				if(grid[x][y] == 1) {
+					cnt--;
 					break;
-				}else {
-					if(dir==4) {//우
-						for(int h=3;h>=1; h--) {
-							col +=h;
-							if(col < N) {
-								if(lake[row][col]==1) {
-									answer--;
-									break;
-								}
-							}
-							else {
-								answer--;
+				}
+				else {
+					if(dir == 1) {//상
+						for(int j = 3; j >= 1; j--) {
+							x -= j;
+							if(x < 0 || grid[x][y] == 1) {
+								cnt--;
 								break;
 							}
-							if(h == 1){
-								lake[row][col]=1;
-							}
+							if(j == 1)
+								grid[x][y] = 1;
 						}
 					}
-					if(dir==3) {//좌
-						for(int h=3;h>=1; h--) {
-							col -=h;
-							if(col >= 0) {
-								if(lake[row][col]==1) {
-									answer--;
-									break;
-								}
-							}
-							else {
-								answer--;
+					else if(dir == 2){//하
+						for(int j = 3; j >= 1; j--) {
+							x += j;
+							if(x >= N || grid[x][y] == 1) {
+								cnt--;
 								break;
 							}
-							if(h == 1){
-								lake[row][col]=1;
-							}
+							if(j == 1)
+								grid[x][y] = 1;
 						}
 					}
-					if(dir==2) {//하
-						for(int v=3; v>=1; v--) {
-							row +=v;
-							if(row < N) {
-								if(lake[row][col]==1) {
-									answer--;
-									break;
-								}
-							}
-							else {
-								answer--;
+					else if(dir == 3) {//좌
+						for(int j = 3; j >= 1; j--) {
+							y -= j;
+							if(y < 0 || grid[x][y] == 1) {
+								cnt--;
 								break;
 							}
-							if(v == 1){
-								lake[row][col]=1;
-							}
+							if(j == 1)
+								grid[x][y] = 1;
 						}
 					}
-					if(dir==1) {//상
-						for(int v=3; v>=1; v--) {
-							row -=v;
-							if(row >= 0) {
-								if(lake[row][col]==1) {
-									answer--;
-									break;
-								}
-							}
-							else {
-								answer--;
+					else if(dir == 4) {//우
+						for(int j = 3; j >= 1; j--) {
+							y += j;
+							if(y >= N || grid[x][y] == 1) {
+								cnt--;
 								break;
 							}
-							if(v == 1){
-								lake[row][col]=1;
-							}
+							if(j == 1)
+								grid[x][y] = 1;
 						}
 					}
 				}
 			}
-			System.out.println("#"+t+" "+(answer-1));
+			System.out.println("#" + t + " " + cnt);
 		}
 	}
 }
