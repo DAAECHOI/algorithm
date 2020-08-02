45a89638c2d059f928802c92b59fa37261851a2b
DAAECHOI <DAAECHOI@DESKTOP-NLHTQLF>
2020-08-03 01:32:29 +0900

    07/30 과제입니다.

--- 0730/BJ2493.java
+++ 0730/BJ2493.java
@@ -2,17 +2,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StringReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Scanner;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class BJ2493 {
 
-	static Stack<Integer> arr;
-	static Stack<Integer> answer;
-	static Stack<Integer> tmp;
+	static Stack<Integer> Tower;
+	static Stack<Integer> tIdx;
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder output = new StringBuilder();
 	static int N;
@@ -20,46 +16,32 @@ public class BJ2493 {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		input = new BufferedReader(new StringReader(src));
-		String line = input.readLine();
-		N = Integer.parseInt(line);
-		arr = new Stack<Integer>();
-		answer = new Stack<Integer>();
-		tmp = new Stack<Integer>();
+		N = Integer.parseInt(input.readLine());
+		Tower = new Stack<Integer>();
+		tIdx = new Stack<Integer>();
 		
-		StringTokenizer tokens = new StringTokenizer(input.readLine(), " ");
+		StringTokenizer tokens = new StringTokenizer(input.readLine());
 
-        while (tokens.hasMoreTokens()) {
-            arr.push(Integer.valueOf(tokens.nextToken()));
-        }
-
-        for(int i = 0; i < N; i++) {
-            tmp.push(arr.pop());
-            int size = tmp.size();
-            if(arr.isEmpty()) {
-            	for(int t = 0; t < size; t++) {
-            		answer.push(0);
-            		tmp.pop();
+        for(int i = 1; i <= N; i++) {
+        	int newT = Integer.valueOf(tokens.nextToken());
+            while(!Tower.isEmpty()) {
+            	if(newT > Tower.peek()) {
+		        	Tower.pop();
+		        	tIdx.pop();
             	}
-            	break;
-            }
-	        if(tmp.peek() < arr.peek()) {
-	        	if(arr.peek()-tmp.peek() < size)
-	        		size = arr.peek()-tmp.peek();
-	        	for(int t = 0; t < size; t++) {
-	    	        if(tmp.peek() < arr.peek()) {
-		        		answer.push(arr.size());
-					    tmp.pop();
-	    	        }
-	    	        else
-	    	        	break;
-	        	}
+            	else {
+            		break;
+            	}
 	        }
+            if(Tower.isEmpty())
+            	output.append("0 ");
+            else
+            	output.append(tIdx.peek() + " ");
+            Tower.push(newT);
+            tIdx.push(i);
         }
-        for(int i = 0; i < N; i++)
-        	System.out.print(answer.pop() + " ");
-        System.out.println();
+        System.out.println(output);
 	}
 
-	static String src = "5\r\n" + 
-			"6 9 5 7 4";
+	static String src = "";
 }
