50c24145fc81c95842662c04f0b77c65fd6ba97b
DAAECHOI <DAAECHOI@DESKTOP-NLHTQLF>
2020-08-07 22:07:11 +0900

    08/07 과제입니다.

--- 0807/BJ1697.java
+++ 0807/BJ1697.java
@@ -1,4 +1,4 @@
-package com.ssafy.algo.d0807;
+package com.ssafy;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -14,33 +14,56 @@ public class BJ1697 {
     static StringBuilder output = new StringBuilder();
     static int N, K;
     static int[] map;
-    static Queue<Integer> queue;
+    static boolean[] visited;
     static int[] dirs = {-1, +1, 2};
+    static Queue<Integer> queue;
     
     public static void main(String[] args) throws IOException {
-        // TODO Auto-generated method stub
-
         input = new BufferedReader(new StringReader(src));
         StringTokenizer tokens = new StringTokenizer(input.readLine(), " ");
         N = Integer.parseInt(tokens.nextToken());
         K = Integer.parseInt(tokens.nextToken());
-        map = new int[100000];
+        map = new int[100001];
+        visited = new boolean[100001];
         
         queue = new LinkedList<>();
-        
+        bfs(N);
     }
     
-    static void bfs() {
-        int size;
-        while(!queue.isEmpty()) {
-            size = queue.size();
-            while(--size >= 0) {
-                Integer front = queue.poll();
-                for(int d = 0; d < dirs.length; d++) {
-                    
-                }
-            }
-        }
+    static void bfs(int start) {
+    	 int size, cnt = 0;
+    	 boolean found = false;
+    	 
+         queue.offer(start);
+         visited[start] = true;
+         
+         while(!queue.isEmpty() && !found) {
+             size = queue.size();
+             while(--size >= 0) {
+                 Integer front = queue.poll();
+                 if(front == K) {
+                	 System.out.print(cnt);
+                	 found = true;
+                	 break;
+                 }
+                 int nf = front;
+                 for(int d = 0; d < dirs.length; d++) {
+                	 if(d == 0)
+                		 nf = front + dirs[d];
+                	 else if(d == 1)
+                		 nf = front + dirs[d];
+                	 else if(d == 2)
+                		 nf = front * dirs[d];
+                	 
+                     if(nf <= 100000 && nf >= 0 && !visited[nf]) {
+                         queue.offer(nf);
+                         visited[nf] = true;
+                     }
+                 }
+             }
+             cnt++;
+         }
     }
+    
     static String src = "5 17";
 }
